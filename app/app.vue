299f4260287d77f2ccba4db3322993a0fcf3792d
youssefelaalem <youssefelaalem82@gmail.com>
2025-09-30 17:55:19 +0300

    refactor: clean up unused code and comments in app.vue and authStore.ts
    - Removed commented-out theme selection code from app.vue.
    - Commented out the entire authStore.ts file to disable its functionality temporarily, preserving the structure for future use.

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,6 @@
 <template>
   <NuxtLayout>
     <v-app>
-      <!-- <v-container>
-        <v-select v-model="value" :items="items" label="Choose theme" variant="outlined" class="w-64"
-          @update:modelValue="switchTheme" />
-        </v-container> -->
         <NuxtPage />
     </v-app>
   </NuxtLayout>
